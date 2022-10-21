d0c96d2364302ff000619f763d8f220be0b5607b
Farouk Bello <faroukk.bello@gmail.com>
2022-10-21 03:38:38 +0100

    Successfully cleaned 'disney_voice_actors' table

--- disney_voice_actors_query.sql
+++ disney_voice_actors_query.sql
@@ -14,3 +14,72 @@ FROM
 
 ALTER TABLE disney_voice_actors
 DROP COLUMN  `index`;
+
+
+
+-- #### 2. CHECK FOR BLANKS #### --
+
+-- Start with the character column
+SELECT DISTINCT
+    `character`
+FROM
+    disney_voice_actors;
+    
+-- 922 Rows? Do we have duplicate entries?
+-- Isolate the entries with characters appearing more than one
+SELECT 
+    dva.*
+FROM
+    disney_voice_actors dva
+WHERE
+    `character` IN (SELECT 
+            Characters_More_Than_One.`character`
+        FROM
+            (SELECT 
+                `character`, COUNT(`character`) AS character_count
+            FROM
+                disney_voice_actors
+            GROUP BY `character`
+            HAVING character_count > 1) AS Characters_More_Than_One)
+ORDER BY movie;
+            
+-- We can see that the voice actors differ for the same character per movie.
+-- So let's do a disntinct count on the voice actors from the result set of
+-- the query above. We should have 23 unique records.
+SELECT DISTINCT
+    COUNT(*)
+FROM
+    disney_voice_actors dva
+WHERE
+    `character` IN (SELECT 
+            Characters_More_Than_One.`character`
+        FROM
+            (SELECT 
+                `character`, COUNT(`character`) AS character_count
+            FROM
+                disney_voice_actors
+            GROUP BY `character`
+            HAVING character_count > 1) AS Characters_More_Than_One);
+-- Perfect!
+-- We have 23 Records. We will leave this column as it is because we'll
+-- be performing our analysis on the character in a particular movie not
+-- the voice actor in the movie.
+
+-- On to the voice_actors column
+SELECT DISTINCT
+    voice_actor
+FROM
+    disney_voice_actors;
+-- We have 655 distinct records with no empty string or blank data. This is
+-- plausible as a character might be portrayed by the same voice_actor from another movie.
+
+-- On to the movies column
+SELECT DISTINCT
+    movie
+FROM
+    disney_voice_actors
+ORDER BY movie;
+-- 139 Rows here only implies we have multiple character in one movie.
+-- This is also plausible hence we leave it as it is.
+
+# --- NOW WE CAN USE THIS TABLE FOR ANALYSIS --- #
